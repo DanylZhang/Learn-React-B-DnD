ea8b2bdcc7298d38ea54909f7a0a477be9f9a8bd
danyl <1475811550@qq.com>
2019-01-07 23:53:59 +0800

    persist reorder task

--- src/index.js
+++ src/index.js
@@ -13,8 +13,37 @@ class App extends React.Component {
     state = initialData;
 
     onDragEnd = result => {
-        // TODO: reorder our column
+        console.log(result);
+        const {draggableId, source, destination} = result;
+        if (!destination) {
+            return;
+        }
+        if (
+            destination.droppableId === source.droppableId &&
+            destination.index === source.index
+        ) {
+            return;
+        }
 
+        const column = this.state.columns[source.droppableId];
+        const newTaskIds = Array.from(column.taskIds);
+        newTaskIds.splice(source.index, 1);
+        newTaskIds.splice(destination.index, 0, draggableId);
+
+        const newColumn = {
+            ...column,
+            taskIds: newTaskIds,
+        };
+
+        const newState = {
+            ...this.state,
+            columns: {
+                ...this.state.columns,
+                [newColumn.id]: newColumn,
+            },
+        };
+
+        this.setState(newState);
     };
 
     render() {
